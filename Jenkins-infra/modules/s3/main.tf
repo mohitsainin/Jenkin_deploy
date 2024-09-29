01a759203b7ac80f7366136ade539c70ce5f4932
mohitsainin <it.mohitsaini@gmail.com>
2024-09-29 21:37:20 +0530

    Update main.tf

--- Jenkins-infra/modules/s3/main.tf
+++ Jenkins-infra/modules/s3/main.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = var.bucket_name
 
   tags = {
-    Name        = "My bucket"
+    Name        = "My jenkins st bucket"
     Environment = "Dev"
   }
 }
