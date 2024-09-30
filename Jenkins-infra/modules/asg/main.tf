25b99f4bf4fd0148b47d4505af9f88882b1ee8c8
mohitsainin <it.mohitsaini@gmail.com>
2024-09-30 16:03:35 +0530

    Update main.tf

--- Jenkins-infra/modules/asg/main.tf
+++ Jenkins-infra/modules/asg/main.tf
@@ -1,19 +1,33 @@
-variable "security_group_id" {
-  type = string
+resource "aws_launch_template" "jenkins_launch_template" {
+  name_prefix   = "jenkins-template"
+  image_id      = var.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [var.security_group_id]
+  key_name      = var.key_name
 }
 
-variable "subnets" {
-  type = list(string)
-}
+resource "aws_autoscaling_group" "jenkins_asg" {
+  name               = "jenkins-asg"
+  vpc_zone_identifier = var.private_subnets
+  desired_capacity    = var.desired_capacity
+  max_size            = var.max_size
+  min_size            = var.min_size
 
-variable "vpc_id" {
-  type = string
-}
+  launch_template {
+    id      = aws_launch_template.jenkins_launch_template.id
+    version = "$Latest"
+    
+  }
+  
+  
+  tag {
+    key                 = "Name"
+    value               = "JenkinsInstance"
+    propagate_at_launch = true
+  }
 
-variable "port" {
-  type = number
-}
+  health_check_type          = "EC2"
+  health_check_grace_period = 300
 
-variable "listener_port" {
-  type = number
+  target_group_arns = [var.target_group_arn]
 }
