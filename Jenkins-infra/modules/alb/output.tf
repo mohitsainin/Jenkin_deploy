9df7a8a54f404f333e745688b870e040e769d5b4
mohitsainin <it.mohitsaini@gmail.com>
2024-10-02 21:00:52 +0530

    Update output.tf

--- Jenkins-infra/modules/alb/output.tf
+++ Jenkins-infra/modules/alb/output.tf
@@ -1,11 +1,11 @@
 output "load_balancer_id" {
-  value = aws_lb.jenkins_lb.id
-}
-
-output "target_group_arn" {
-  value = aws_lb_jenkins_group_tg.arn
+  value = aws_lb.jenkins_lb.id  # Ensure this points to the correct resource
 }
 
 output "listener_arn" {
-  value = aws_lb_listener.alb.arn
+  value = aws_lb_listener.alb.arn  # Ensure this points to the correct resource
+}
+
+output "target_group_arn" {
+  value = aws_lb_target_group.jenkins_tg.arn
 }
