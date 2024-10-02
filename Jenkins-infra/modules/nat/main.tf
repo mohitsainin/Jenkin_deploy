9d3513272b2453dc12bd8365d86d03723a5b1497
mohitsainin <it.mohitsaini@gmail.com>
2024-10-02 21:15:06 +0530

    Update main.tf

--- Jenkins-infra/modules/nat/main.tf
+++ Jenkins-infra/modules/nat/main.tf
@@ -1,4 +1,4 @@
-resource "aws_nat_gateway" "NAT" {
+resource "aws_nat_gateway" "nat" {
   allocation_id = var.eip_allocation_id
   subnet_id     = var.subnet_id
   tags          = merge(var.tags, { "Name" = "NAT Gateway" })
