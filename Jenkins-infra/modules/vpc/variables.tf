c27ebe19fa29fd546d1e85c6e5596e3afa3b2807
mohitsainin <it.mohitsaini@gmail.com>
2024-10-04 11:38:33 +0530

    Update variables.tf

--- Jenkins-infra/modules/vpc/variables.tf
+++ Jenkins-infra/modules/vpc/variables.tf
@@ -1,7 +1,24 @@
-variable "cidr_block" {
+variable "vpc_id" {
   type = string
 }
 
-variable "vpc_name" {
+variable "internet_gateway_id" {
   type = string
+}
+
+variable "nat_gateway_id" {
+  type = string
+}
+
+variable "public_subnet_ids" {
+  type = list(string)
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
 }
