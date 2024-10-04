7e04a9d9bbda8a7e584cf646af0795e56072ce53
mohitsainin <it.mohitsaini@gmail.com>
2024-10-04 11:38:07 +0530

    Update output.tf

--- Jenkins-infra/modules/vpc/output.tf
+++ Jenkins-infra/modules/vpc/output.tf
@@ -1,7 +1,7 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
+output "vpc_peering_id" {
+     value = aws_vpc_peering_connection.vpc_peering.id
+  
 }
-
-output "vpc_cidr_block" {
-  value = aws_vpc.main.cidr_block
+output "vpc_id-01" {
+  value = data.aws_vpc.default_vpc.id
 }
