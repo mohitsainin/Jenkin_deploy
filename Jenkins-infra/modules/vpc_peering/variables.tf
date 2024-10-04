8760f001e227b6bb7dbe50c72469e1a335caa0db
mohitsainin <it.mohitsaini@gmail.com>
2024-10-05 01:10:40 +0530

    Update variables.tf

--- Jenkins-infra/modules/vpc_peering/variables.tf
+++ Jenkins-infra/modules/vpc_peering/variables.tf
@@ -1,35 +1,16 @@
-
+variable "region_name" {
+  default = "us-east-1"
+}
 
 variable "vpc_id" {
   type = string
 }
-variable "region_name" {
-  type = string
-  default = "us-east-1"
-  
-}
 
-variable "vpc_peering_name" {
-  type = string
-  default = "jenkins_peering"
-}
-
-
-variable "cidr_range" {
-  type = string
-  default = "172.16.0.0/22"
-}
-
-variable"public_route_table_id" {
-  type = string
-}
-
-variable "private_route_table_id" {
- type = string
+variable "vpc_cidr" {
+    type = string
 }
 
 variable "default_vpc_cidr" {
   type = string
   default = "172.31.0.0/16"
-
 }
